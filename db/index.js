081e3b3ee9eef5fd3118921cebbf76f7e66c54f2
Lily Chen <linglin1638078@gmail.com>
2022-09-24 12:41:31 -0400

    final

--- db/index.js
+++ db/index.js
@@ -5,9 +5,65 @@ const DB_URL = process.env.DB_URL || "postgres://localhost:5432/seq_intro";
 const db = new Sequelize(DB_URL);
 
 const User = db.define('user', {
-    name:{
-        type:Sequelize.STRING,
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        //can set constraint
+        /*validate: {
+            min: 0,
+            max: 20
+        },*/
+        
+        //unique: true - make sure no one enter the same firstName twice
+
+        //return the way the user want to look-not actually changing the field data
+        get() {
+            const currName = this.getDataValue('firstName');
+            const firstLetter = currName[0].toUpperCase();
+            const otherLetters = currName.slice(1);
+            return firstLetter + otherLetters;
+        },
+        //seting the data that is going into the database
+        set(value) {
+            const lowerCaseName = value.toLowerCase();
+            this.setDataValue('firstName', lowerCaseName)
+        }
+    },
+
+    lastName: {
+        type: Sequelize.STRING,
         allowNull: false
+    },
+
+    fullName: {
+        //virtual field that exited in databse- not creating new field, but use get() to access info in other field to manipulate
+        //will manipulate it before getting back to user
+        type: Sequelize.VIRTUAL,
+        get() {
+            const firstName = this.getDataValue('firstName');
+            const lastName = this.getDataValue('lastName');
+            return `${firstName} ${lastName}`;
+        }
+    },
+
+    /*role: {
+            type: Sequelize.STRING,
+            set(value) {
+                if (value !== 'mod' || value !== 'admin' || value !== 'user') {
+                    //set it
+                }
+                else{
+                    //don't
+                };
+            }
+        }*/
+        //ignore any role other than the below three
+    role: {
+        type: Sequelize.ENUM([
+            'mod',
+            'admin',
+            'user'
+        ]),
     }
 })
 
@@ -23,7 +79,7 @@ const Post = db.define('post',{
     
 });
 
-User.hasMany(Post);
+User.hasMany(Post);//UserId and PostId filed is automatically created when the relationship is created
 Post.belongsTo(User);
 
 module.exports = {
--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+const { user } = require('pg/lib/defaults');
 const {
     db, User, Post
 } = require('./db');
@@ -8,13 +9,41 @@ const seedDb = async () => {
     //db is client
     await db.sync({ force: true, logging: false });
 
+    //will run way faster than write inidivual async await for each user
+    /*const users = [
+        {
+            firstName: 'Louis',
+            lastName: 'RaBeno',
+
+            role: 'mod'
+        },
+        {
+            firstName: "Ben",
+            lastName: "Odisho",
+            role: 'user'
+    
+        }
+    ];
+    //all promises will be saved in an array before returning back
+    const Promises = users.map((user) => User.create(user));
+    //async action that wait for each single element in the array to be finished
+    const[louis, ben] = await Promise.all(Promises);*/
+
+    const start = new Date()
+
+
     //create user
     const Louis = await User.create({
-        name: "Louis"
+        firstName: "Louis",
+        lastName: "Rabeno",
+        role:'admin'
     });
 
     const Ben = await User.create({
-        name: "Ben"
+        firstName: "Ben",
+        lastName: "Odisho",
+        role:'user'
+
     });
 
     //create post for user
@@ -30,9 +59,12 @@ const seedDb = async () => {
         userId: Ben.id
     });
 
+    const end = new Date();
+    console.log(end - start);
+    
     //find all users
     console.log(
-        (await User.findAll()).map(user => user.name)
+        (await User.findAll()).map(user => user.role)
     );
 
     console.log(
